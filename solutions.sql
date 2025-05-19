bcbc60d320b2de295cab8c5f255eadec0b6578f6
Desiree Maestri <maestri.desiree@gmail.com>
2025-05-19 16:25:19 +0200

    adjust syntax2

--- solutions.sql
+++ solutions.sql
@@ -16,8 +16,7 @@ ORDER BY amount ASC
 LIMIT 3;
 
 --4.What are the possible values for status, ordered alphabetically in ascending order in the loan table?
-SELECT status from loan
-GROUP BY status 
+SELECT DISTINCT status from loan
 ORDER BY status ASC;
 
 --5.What it is the loan_id of the highest payment received in the loan table?
@@ -37,8 +36,7 @@ ORDER BY amount ASC
 LIMIT 5;
 
 --8. What are the unique values of k_symbol in the order table?
-SELECT k_symbol FROM `order`
-GROUP BY k_symbol;
+SELECT DISTINCT k_symbol FROM `order`
 
 --9. In the order table, what are the order_ids of the client with the account_id 34?
 SELECT order_id FROM `order`
@@ -46,7 +44,7 @@ WHERE account_id = 34;
 
 --10.In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)?
 SELECT account_id FROM `order`
-WHERE order_id > 29540 AND order_id < 29561
+WHERE order_id > 29540 AND < 29561
 GROUP BY account_id;
 
 --11.In the order table, what are the individual amounts that were sent to (account_to) id 30067122?
@@ -62,7 +60,8 @@ LIMIT 10;
 --13. In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
 SELECT district_id, COUNT(*) FROM client
 WHERE district_id < 10
-GROUP BY district_id;
+GROUP BY district_id
+ORDER BY district_id ASC;
 
 --14. In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
 SELECT type, count(*) FROM card
@@ -70,7 +69,7 @@ GROUP BY type
 ORDER BY count(*) DESC;
 
 --15. Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts.
-SELECT account_id, amount FROM loan
+SELECT account_id, SUM(amount) FROM loan
 GROUP BY account_id
 ORDER BY amount DESC
 LIMIT 10;
